dd4766c180feca62e10a261740a554767c8b2fbc
pcbal <pcbalakrishnan@gmail.com>
2026-02-20 17:27:40 +0530

    Initial commit

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+import React from 'react'
+
+interface Props {
+  className?: string
+  loading?: 'lazy' | 'eager'
+  priority?: 'auto' | 'high' | 'low'
+}
+
+export const Logo = (props: Props) => {
+  const { loading: loadingFromProps, priority: priorityFromProps, className } = props
+
+  const loading = loadingFromProps || 'lazy'
+  const priority = priorityFromProps || 'low'
+
+  return (
+    /* eslint-disable @next/next/no-img-element */
+    <img
+      alt="NetNetLead Logo"
+      width={193}
+      height={34}
+      loading={loading}
+      fetchPriority={priority}
+      decoding="async"
+      className={clsx('max-w-[29.375rem] w-full h-[70px]', className)}
+      src="/assets/NetNetLead_crop.jpeg"
+    />
+  )
+}
